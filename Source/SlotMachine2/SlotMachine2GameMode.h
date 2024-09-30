7b2928a1bd543c75a23af41daa9e75a325927b8a
Darkchaton <ameliadulac12345@gmail.com>
2024-09-30 16:46:51 -0400

    first commit

---
+++ Source/SlotMachine2/SlotMachine2GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlotMachine2GameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASlotMachine2GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASlotMachine2GameMode();
+};
+
+
+
